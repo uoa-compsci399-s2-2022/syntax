9d2eb720da74e81a206808de747bdda0df919bfa
Lily Howan <lily.howan@gmail.com>
2022-09-17 22:30:36 +1200

    Add https prefix to link function URL if it is not present

--- components/editor/Menubar.js
+++ components/editor/Menubar.js
@@ -108,6 +108,9 @@ export default ({ editor }) => {
       editor.chain().focus().unsetLink().run();
     } else {
       const url = prompt("Enter URL");
+      if (!(url.match(/https:\/\/*/) || url.match(/http:\/\/*/))) {
+        url = 'https://' + url
+      }
       if (url) {
         editor
           .chain()
--- components/editor/Tiptap.js
+++ components/editor/Tiptap.js
@@ -87,6 +87,7 @@ export default function () {
         }
       }),
       Link.configure({
+
         HTMLAttributes: {
           class: "editor-link"
         }
