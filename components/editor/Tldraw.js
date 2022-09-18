2b6589947428a7a11c2c81a166b120ad27c5e684
bonjuruuu <sarah.kyounghui@gmail.com>
2022-09-18 16:04:32 +1200

    add reset cursor for correct pointer

--- components/editor/Tldraw.js
+++ components/editor/Tldraw.js
@@ -4,10 +4,19 @@ import { Modal, Button } from "@nextui-org/react"
 
 const DrawingModal = ({ open, closeHandler }) =>{
     const [app, setApp] = useState()
+    const [width, setWidth] = useState("1%")
 
-        const handleMount = useCallback((app) => {
-            setApp(app)
-        }, [])
+    const handleMount = useCallback((app) => {
+        setApp(app)
+        resetCursor()
+    }, [])
+
+    const resetCursor = () => {
+        setWidth("1%")
+        setTimeout(function() { //Start the timer
+            setWidth("100%") //After 0.1 second
+        }, 100)
+    }
 
     async function saveAndClose(){
         app?.selectNone()
@@ -21,24 +30,14 @@ const DrawingModal = ({ open, closeHandler }) =>{
         closeHandler(files)
     }
     return (
-        <Modal width open={open} onClose={closeHandler} css={{ 
-            margin: "auto", 
-            '@xs': "width: 100%", 
-            '@sm': "width: 75%", 
-            '@md': "width: 50%",
-            '@lg': "width: 50%",
-            display: 'flex'
-            }}>
-            <Modal.Header>
-                <h3>Drawing</h3>
-            </Modal.Header>
+        <Modal noPadding width={width} open={open} onClose={closeHandler} onOpen={()=>resetCursor()}>
             <Modal.Body>
                 <div
                     style={{
-                    position: 'relative',
-                    width: '100%',
-                    height: '500px',
-                    overflow: 'hidden',
+                        position: 'relative',
+                        width: '100%',
+                        height: '500px',
+                        overflow: 'hidden',
                     }}
                     >
                         <Tldraw onMount={handleMount} showMenu={false} showPages={false} showMultiplayerMenu={false}/> 
