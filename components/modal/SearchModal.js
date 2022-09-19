90c5d0cda1ca09e032f1dc12d14b4cd653432ffe
Lily Howan <lily.howan@gmail.com>
2022-09-19 19:52:15 +1200

    Add title, content and code filter buttons

--- components/modal/SearchModal.js
+++ components/modal/SearchModal.js
@@ -1,9 +1,20 @@
-import { Modal, Input, Button, Dropdown, Container } from "@nextui-org/react";
+import {
+  Modal,
+  Input,
+  Button,
+  Dropdown,
+  Container,
+  Row,
+  Text
+} from "@nextui-org/react";
 import { useState, useEffect } from "react";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 
 const SearchModal = ({ open, closeHandler }) => {
-  const [selectedSort, setSelectedSort] = useState();
-  const [selectedGroup, setSelectedGroup] = useState();
+  const [selectedSort, setSelectedSort] = useState("title-asc");
+  const [titleChecked, setTitleChecked] = useState(true);
+  const [contentChecked, setContentChecked] = useState(false);
+  const [codeChecked, setCodeChecked] = useState(false);
 
   const sortOptions = {
     "title-asc": "Title (ascending)",
@@ -13,10 +24,16 @@ const SearchModal = ({ open, closeHandler }) => {
     "created-asc": "Created (ascending)",
     "created-desc": "Created (descending)"
   };
-  const defaultSort = "title-asc";
+
+  const exampleNotes = [
+    { title: "Test 1", updatedAt: "Date" },
+    { title: "Test 2", updatedAt: "Date" },
+    { title: "Test 3", updatedAt: "Date" }
+  ];
 
   return (
     <Modal
+      blur
       closeButton
       open={open}
       onClose={closeHandler}
@@ -25,45 +42,68 @@ const SearchModal = ({ open, closeHandler }) => {
     >
       <Modal.Header>Advanced Search</Modal.Header>
       <Modal.Body>
-        <Input clearable animated={false} label="Title" placeholder="Title" />
         <Input
           clearable
+          aria-label="Advanced Search Bar"
+          placeholder="Search content..."
+          type="search"
           animated={false}
-          label="Note Content"
-          placeholder="Note Content"
+          contentLeft={
+            <MagnifyingGlassIcon style={{ height: "var(--icon-size)" }} />
+          }
         />
-        <Container display="flex" css={{ padding: "0", gap: "10px" }}>
-          <Dropdown>
-            <Dropdown.Button bordered css={{ flex: "1" }}>
-              In Group: {selectedGroup ? selectedGroup : "Any"}
-            </Dropdown.Button>
-            <Dropdown.Menu
-              aria-label="Group Options"
-              onAction={setSelectedGroup}
-            >
-              <Dropdown.Item key="group1">Group 1</Dropdown.Item>
-              <Dropdown.Item key="group2">Group 2</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-          <Dropdown>
-            <Dropdown.Button bordered css={{ flex: "1" }}>
-              Sort by: {selectedSort ? sortOptions[selectedSort] : sortOptions[defaultSort]}
-            </Dropdown.Button>
-            <Dropdown.Menu aria-label="Sort Options" onAction={setSelectedSort}>
+        <Container
+          display="flex"
+          wrap="nowrap"
+          css={{ padding: "0", gap: "10px" }}
+        >
+          <Button
+            auto
+            bordered={titleChecked ? false : true}
+            onPress={() => setTitleChecked(!titleChecked)}
+          >
+            Title
+          </Button>
+          <Button
+            auto
+            bordered={contentChecked ? false : true}
+            onPress={() => setContentChecked(!contentChecked)}
+          >
+            Content
+          </Button>
+          <Button
+            auto
+            bordered={codeChecked ? false : true}
+            onPress={() => setCodeChecked(!codeChecked)}
+          >
+            Code
+          </Button>
+        </Container>
+        <hr style={{ marginBottom: "0" }} />
+        <Dropdown>
+          <Dropdown.Button
+            light
+            animated="false"
+            css={{ width: "min-content", padding: "0" }}
+          >
+            {sortOptions[selectedSort]}
+          </Dropdown.Button>
+          <Dropdown.Menu aria-label="Sort Options" onAction={setSelectedSort}>
             {Object.keys(sortOptions).map((option) => (
-              <Dropdown.Item key={option}>
-                {sortOptions[option]}
-              </Dropdown.Item>
+              <Dropdown.Item key={option}>{sortOptions[option]}</Dropdown.Item>
             ))}
-            </Dropdown.Menu>
-          </Dropdown>
+          </Dropdown.Menu>
+        </Dropdown>
+        <Container css={{ padding: "0" }}>
+          {exampleNotes.map((note, index) => (
+            <Row>
+              <Text h4>{note.title}</Text>
+              <Text>{note.updatedAt}</Text>
+            </Row>
+          ))}
         </Container>
       </Modal.Body>
-      <Modal.Footer>
-        <Button auto bordered onClick={closeHandler}>
-          Search
-        </Button>
-      </Modal.Footer>
+      <Modal.Footer></Modal.Footer>
     </Modal>
   );
 };
