fb43c5184b3ab8598ee1eefd189cdf347694231a
momor1n <ljane.dio@gmail.com>
2022-09-22 21:19:14 +1200

    added a border

--- components/editor/CodeMirrorExtension.js
+++ components/editor/CodeMirrorExtension.js
@@ -7,7 +7,7 @@ import { python } from "@codemirror/lang-python";
 import { StreamLanguage } from "@codemirror/language"
 import { clike } from "@codemirror/legacy-modes/mode/clike"
 import { useEffect, useState, useRef } from "react";
-import { useTheme, Button, Spacer, Dropdown } from "@nextui-org/react";
+import { useTheme, Button, Spacer, Dropdown, Card } from "@nextui-org/react";
 import {
     NodeViewWrapper
 } from "@tiptap/react";
@@ -64,8 +64,10 @@ export const Extension = ({
     }, [type]);
 
     return ( <NodeViewWrapper>
-        <div className = "maindiv" >
 
+        <Card variant='bordered' css={{$$cardColor: 'rgba(255,255,255,0.0)'}}>
+        <Card.Body>
+        <div className = "maindiv" >
         <Dropdown>
         <Dropdown.Button light css = {
             { tt: 'capitalize' }
@@ -83,16 +85,20 @@ export const Extension = ({
             }</Dropdown.Menu> 
         </Dropdown >
 
-        <refEditor ref = { refEditor } />   
+        <refEditor ref = { refEditor }/>   
         <Spacer y = { 0.5 }/>  
         <Button bordered responsive auto ghost size = 'xs'
         onClick = {
             () => run()
-        } > Run Code </Button>  
+        } > Run Code </Button> 
+
+        <Spacer y ={ 0.5 }/>
         <div className = "output" >
         <span> { result } </span>  
         </div> 
-        </div> 
+        </div>
+        </Card.Body>
+        </Card>
 
         </NodeViewWrapper >
     );
