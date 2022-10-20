12f8e605c22f9d63fcb01c40027baa8b8864268f
Lily Howan <lily.howan@gmail.com>
2022-10-20 17:50:04 +1300

    Style code blocks using new design

--- components/editor/CodeMirrorExtension.js
+++ components/editor/CodeMirrorExtension.js
@@ -9,11 +9,11 @@ import {
 	Spacer,
 	Dropdown,
 	Container,
-	Collapse
+	Row
 } from "@nextui-org/react";
 import { NodeViewWrapper } from "@tiptap/react";
 import { TIO, LANGUAGES as TioLanguages } from "@/node/tio";
-import { oneDark } from "@codemirror/theme-one-dark";
+import { oneDark, color as oneDarkColors } from "@codemirror/theme-one-dark";
 import { tomorrow } from "thememirror";
 
 const themeExtensions = {
@@ -36,6 +36,7 @@ export const Extension = ({
 	const refEditor = useRef(null);
 	const [language, setLanguage] = useState();
 	const [input, setInput] = useState([]);
+	const [codeOutputOpen, setCodeOutputOpen] = useState(false);
 	const [systemInfo, setSystemInfo] = useState(time == "" ? true : false);
 	const { checked, type } = useTheme();
 
@@ -84,54 +85,107 @@ export const Extension = ({
 			<Container
 				contentEditable={false}
 				css={{
-					background: "$backgroundContrast",
+					background: type === "dark" ? oneDarkColors.background : "white",
+					color: type === "dark" ? oneDarkColors.ivory : "#4D4D4C",
 					borderRadius: "$lg",
-					padding: "$lg $sm",
+					padding: "$md $sm",
 					margin: "$10 0",
-					border: "1px solid $border"
+					border: type === "dark" ? "" : "1px solid $border"
 				}}
 			>
-				<Dropdown>
-					<Dropdown.Button
-						light
-						ripple={false}
-						css={{ tt: "capitalize", paddingLeft: "0", zIndex: 0 }}
-					>
-						{langDict[lang]}
-					</Dropdown.Button>
-					<Dropdown.Menu
-						items={TioLanguages}
-						onAction={(key) => {
-							updateAttributes({ language: TioLanguages[key] });
-						}}
-					>
-						{TioLanguages.map((lang, index) => (
-							<Dropdown.Item key={index} value={lang}>
-								{langDict[lang]}
-							</Dropdown.Item>
-						))}
-					</Dropdown.Menu>
-				</Dropdown>
-
+				<Row
+					justify="space-between"
+					align="center"
+					css={{ marginBottom: "0.5rem" }}
+				>
+					<Dropdown>
+						<Dropdown.Button
+							light
+							ripple={false}
+							css={{
+								tt: "capitalize",
+								padding: "0",
+								lineHeight: "0",
+								height: "min-content",
+								zIndex: 0,
+								color: type === "dark" ? oneDarkColors.ivory : "#4D4D4C"
+							}}
+						>
+							{langDict[lang]}
+						</Dropdown.Button>
+						<Dropdown.Menu
+							items={TioLanguages}
+							onAction={(key) => {
+								updateAttributes({ language: TioLanguages[key] });
+							}}
+						>
+							{TioLanguages.map((lang, index) => (
+								<Dropdown.Item key={index} value={lang}>
+									{langDict[lang]}
+								</Dropdown.Item>
+							))}
+						</Dropdown.Menu>
+					</Dropdown>
+					<Button auto size="sm" onPress={() => run()} css={{ zIndex: 0 }}>
+						Run
+					</Button>
+				</Row>
 				<div ref={refEditor} />
 				<Spacer y={0.5} />
-				<Button auto size="xs" onClick={() => run()} css={{ zIndex: 0 }}>
-					Run Code
-				</Button>
+				<Spacer y={0.5} />
 
-				<Spacer y={0.5} />
-				<hr></hr>
-				<Spacer y={0.5} />
-				<div style={{ whiteSpace: "pre-line" }}>
-					<Collapse.Group accordion={false}>
-						<Collapse disabled={systemInfo} title="Output:">
-							<span> {result} </span>
-						</Collapse>
-						<Collapse disabled={systemInfo} title="System Info:">
-							{time}
-						</Collapse>
-					</Collapse.Group>
-				</div>
+				{result && (
+					<>
+						<hr></hr>
+						<Spacer y={0.5} />
+						<Row justify="space-between" css={{ whiteSpace: "pre-line" }}>
+							<details
+								style={{ background: "transparent" }}
+							>
+								<summary
+									style={{
+										display: "flex",
+										alignItems: "center",
+										gap: "0.3rem"
+									}}
+									onClick={() => setCodeOutputOpen(!codeOutputOpen)}
+								>
+									{codeOutputOpen ? (
+										<svg
+											fill="none"
+											height="14"
+											viewBox="0 0 24 24"
+											width="14"
+											xmlns="http://www.w3.org/2000/svg"
+										>
+											<path
+												d="M17.9188 8.17969H11.6888H6.07877C5.11877 8.17969 4.63877 9.33969 5.31877 10.0197L10.4988 15.1997C11.3288 16.0297 12.6788 16.0297 13.5088 15.1997L15.4788 13.2297L18.6888 10.0197C19.3588 9.33969 18.8788 8.17969 17.9188 8.17969Z"
+												fill="currentColor"
+											></path>
+										</svg>
+									) : (
+										<svg
+											fill="none"
+											height="14"
+											viewBox="0 0 24 24"
+											width="14"
+											xmlns="http://www.w3.org/2000/svg"
+											transform="rotate(-90)"
+										>
+											<path
+												d="M17.9188 8.17969H11.6888H6.07877C5.11877 8.17969 4.63877 9.33969 5.31877 10.0197L10.4988 15.1997C11.3288 16.0297 12.6788 16.0297 13.5088 15.1997L15.4788 13.2297L18.6888 10.0197C19.3588 9.33969 18.8788 8.17969 17.9188 8.17969Z"
+												fill="currentColor"
+											></path>
+										</svg>
+									)}
+									Output
+								</summary>
+								{result}
+							</details>
+							{time.match(/(Real time: (.*) s)/g)}
+						</Row>
+					</>
+				)}
 			</Container>
 		</NodeViewWrapper>
 	);
