6471786a1937436e29eeb140d5ffdfbde9316117
HFx6 <haydenfx6@gmail.com>
2022-10-21 23:26:29 +1300

    made editor viable outside of the tiptap.js context. extensions unrelated to specific functionality are now imported externally

---
+++ components/editor/baseExtensions.js
@@ -0,0 +1,44 @@
+import StarterKit from "@tiptap/starter-kit";
+import BulletList from "@tiptap/extension-bullet-list";
+import Underline from "@tiptap/extension-underline";
+import Superscript from "@tiptap/extension-superscript";
+import Subscript from "@tiptap/extension-subscript";
+import Youtube from "@tiptap/extension-youtube";
+import Link from "@tiptap/extension-link";
+import Placeholder from "@tiptap/extension-placeholder";
+import { TipTapCustomImage } from "@/node/Image";
+import { Drawing } from "@/node/Drawing";
+import { CodeBlockNode } from './CodeMirrorNode';
+import { DebounceSave } from './DebounceSaveExtension';
+
+export const baseExtensions = () => {
+	return [
+		StarterKit.configure({
+			codeBlock: false,
+			bulletList: false
+		}),
+		Underline,
+		Superscript,
+		Subscript,
+		Youtube,
+		BulletList.configure({
+			HTMLAttributes: {
+				class: "editor-ul"
+			}
+		}),
+		Link.configure({
+			HTMLAttributes: {
+				class: "editor-link"
+			}
+		}),
+		CodeBlockNode,
+		Drawing().configure({
+			HTMLAttributes: {
+				class: 'drawing'
+			}
+		}),
+		Placeholder.configure({
+			placeholder: "Select a note or start typing here to get started..."
+		})
+	]
+}
