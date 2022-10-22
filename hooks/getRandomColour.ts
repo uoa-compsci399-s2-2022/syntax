ac92071a0f138f188be3264398574f3dde2b4bfa
momor1n <ljane.dio@gmail.com>
2022-10-22 19:52:49 +1300

    fixed up colors

--- hooks/getRandomColour.ts
+++ hooks/getRandomColour.ts
@@ -1,26 +1,15 @@
 const colours = [
-    '#e6194B', 
-    '#3cb44b', 
-    '#ffe119', 
-    '#4363d8', 
-    '#f58231', 
-    '#911eb4', 
-    '#42d4f4', 
-    '#f032e6', 
-    '#bfef45', 
-    '#fabed4', 
-    '#469990', 
-    '#dcbeff', 
-    '#9A6324', 
-    '#fffac8', 
-    '#800000', 
-    '#aaffc3', 
-    '#808000', 
-    '#ffd8b1', 
-    '#000075', 
-    '#a9a9a9'
+    '#667080',
+    '#E6194B',
+    '#F58231',
+    '#CCB100',
+    '#3CB44B',
+    '#4747FF',
+    '#911EB4',
+    '#F032E6',
+    '#808000',
+    '#8F4700'
 ]
-
 const getRandomColour = () => {
     return colours[Math.floor(Math.random() * colours.length)]
 }
